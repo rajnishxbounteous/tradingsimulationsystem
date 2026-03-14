8d7b1267f94ca4d8576dca4c740f8787bd1a1fba
Rajnish Kumar <rajnish.kumar@bounteous.com>
2026-03-14 18:53:57 +0530

    Created tradingsimulationsystem and added app.prop file and dependencies in maven project

---
+++ tradingsimulationsystem/src/test/java/com/example/tradingsimulationsystem/TradingsimulationsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tradingsimulationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingsimulationsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
